7f06b6e4d20e45c339bf69410648f34511e435a9
Brian McIntosh <brian@mixinteractive.com>
2023-01-08 11:16:51 -0500

    Initial Commit

---
+++ TundsDevApproach/TundsDevApproachApp.swift
@@ -0,0 +1,17 @@
+//
+//  TundsDevApproachApp.swift
+//  TundsDevApproach
+//
+//  Created by Brian McIntosh on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TundsDevApproachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
