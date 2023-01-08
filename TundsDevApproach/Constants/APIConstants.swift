d9797a0d97ea0c324a7b37782fbb3d30446dc186
Brian McIntosh <brian@mixinteractive.com>
2023-01-08 13:25:52 -0500

    Add APIConstants

---
+++ TundsDevApproach/Constants/APIConstants.swift
@@ -0,0 +1,24 @@
+//
+//  APIConstants.swift
+//  TundsDevApproach
+//
+//  Created by Brian McIntosh on 1/8/23.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let baseUrl = "https://animechan.vercel.app"
+    // "/api/random" is the endpoint
+}
+
+/*
+ Why didn't we use a struct? (...essentially the same)
+ 1. don't want someone to instantiate this object
+ 2. you can't really create an instance of an enum
+ 3. like a namespace for our properties
+*/
+
+//struct Constants {
+//    static let baseUrl = "https://animechan.vercel.app"
+//}
